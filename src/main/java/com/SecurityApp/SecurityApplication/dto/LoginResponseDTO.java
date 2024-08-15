1e2c36ca8d05d269a5e42e523a048cf875098823
Ashutosh <ashu.ksing1018@gmail.com>
2024-08-16 01:24:05 +0530

    spring security refresh

---
+++ src/main/java/com/SecurityApp/SecurityApplication/dto/LoginResponseDTO.java
@@ -0,0 +1,15 @@
+package com.SecurityApp.SecurityApplication.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class LoginResponseDTO {
+
+    private Long id;
+    private String accessToken;
+    private String refreshToken;
+}
